227fecce1558e9cefbdbc9a269f6184b1ba114da
thalles <thallesgalv@gmail.com>
2020-11-02 19:43:25 -0300

    começo do validador

--- js/form_validacao.js
+++ js/form_validacao.js
@@ -1,40 +1,47 @@
 function validacao() {
   
-  function campoBranco() {
+  function infoCurta() {
     document.querySelectorAll('input').forEach((item) => {
-      if (item.value == "") {
-        alert(item.name + " está vazio!")
+      if (item.value.length < 3) {
+        alert("Campo têm poucos caracteres!")
+        item.style.border = '5px solid red';
+        item.style.backgroundColor = '#FFC0B5';
       }
     })
 
     if (document.querySelector('textarea').value == "") {
-      alert("Campo de mensagem vazio!")
+      alert("Campo de mensagem têm poucos caracteres!")
+      item.style.border = '5px solid red';
+      item.style.backgroundColor = '#FFC0B5';
     }
   }
 
   function validaNome() {
-    if (document.querySelector('[type=text]').value.length < 3) {
-      alert('Nome muito curto!')
+    if (!isNaN(document.querySelector('[type=text]').value)) {
+      alert("Campo nome não aceita números")
+      document.querySelector('[type=text]').style.border = '5px solid red';
+      document.querySelector('[type=text]').style.backgroundColor = '#FFC0B5';
     }
   }
 
   function validaTel() {
-    if (isNaN(document.querySelector('[type=tel]').value)) {
-      alert("Campo telefone só aceita números")
+    if (isNaN(document.querySelector('[type=tel]').value) || document.querySelector('[type=tel]').value.length < 8) {
+      alert("Digite um telefone válido")
+      document.querySelector('[type=tel]').style.border = '5px solid red';
+      document.querySelector('[type=tel]').style.backgroundColor = '#FFC0B5';
     }
   }
 
   function validaEmail() {
-    if (!document.querySelector('[type=email]').value.includes("@")) {
+    if (!document.querySelector('[type=email]').value.includes("@") || !document.querySelector('[type=email]').value.endsWith('.com')) {
       alert("Digite um e-mail válido")
+      document.querySelector('[type=email]').style.border = '5px solid red';
+      document.querySelector('[type=email]').style.backgroundColor = '#FFC0B5';
     }
   }
-
   
+  infoCurta();
   validaNome();
-  campoBranco();
   validaTel();
   validaEmail();
 }
-
-document.querySelector('[type=submit]').addEventListener('click', validacao)
